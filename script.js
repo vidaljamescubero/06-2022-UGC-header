1f07dda4ccfcb40b82e9ad55a667715773b41679
vidal james cubero <cubero.vj@gmail.com>
2022-06-22 04:56:33 +0800

    hard coded

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let count = 0;
+let mobCount = 0;
 
 function moveRight() {
     if (count == 7) {
-        count = 0
+        count = 0;
     } else {
-        count++
-    }
+        count++;
+    };
 
     let value = count * 11.18;
 
@@ -16,10 +17,10 @@ function moveRight() {
 
 function moveLeft(){
     if (count == 0) {
-        count = 7
+        count = 7;
     } else {
-        count--
-    }
+        count--;
+    };
 
     let value = count * 11.18;
 
@@ -27,3 +28,38 @@ function moveLeft(){
         transform: translateX(-${value}%);
     `
 }
+
+function moveRightMobile() {
+    if (mobCount === 6) {
+        mobCount = 0
+    } else {
+        mobCount++;
+    };
+
+    let value = mobCount * 12.588;
+
+    document.querySelector(".ugc-header-mobile-cards").style.cssText = `
+        transform: translateX(-${value}%);
+    `
+}
+
+function moveLeftMobile() {
+    if (mobCount === 0) {
+        mobCount = 6
+    } else {
+        mobCount--;
+    };
+
+    let value = mobCount * 12.588;
+
+    document.querySelector(".ugc-header-mobile-cards").style.cssText = `
+        transform: translateX(-${value}%);
+    `
+}
+
+window.addEventListener('resize', function (){
+    count = 0;
+    mobCount = 0;
+    document.querySelector(".ugc-header-cards").style.cssText = ``;
+    document.querySelector(".ugc-header-mobile-cards").style.cssText = ``;
+})
